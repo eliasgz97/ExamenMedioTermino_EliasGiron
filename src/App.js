5432740efc9a2f8d1af2eb5e7fba00ed9da62b27
eliasgz97 <47019721+eliasgz97@users.noreply.github.com>
2020-08-22 20:11:30 -0600

    Funcion insertar agregada

--- src/App.js
+++ src/App.js
@@ -20,25 +20,25 @@ function App() {
   });
   const seleccionarElemento = (elemento, caso) => {
     setElementoSeleccionado(elemento);
-    (caso === 'Eliminar')&&setModalEliminar(true)
+    (caso === 'Eliminar') && setModalEliminar(true)
   }
-  const manejarCambio=e=>{
-    const {name, value}=e.target;
-    setElementoSeleccionado((prevState)=>({
+  const manejarCambio = e => {
+    const { name, value } = e.target;
+    setElementoSeleccionado((prevState) => ({
       ...prevState,
       [name]: value
     }))
 
   }
   const eliminar = () => {
-    setData(data.filter(elemento=>elemento.Apunte!==elementoSeleccionado.Apunte));
+    setData(data.filter(elemento => elemento.Apunte !== elementoSeleccionado.Apunte));
     setModalEliminar(false);
   }
-  const abrirModalInsertar = ()=>{
+  const abrirModalInsertar = () => {
     setElementoSeleccionado(null);
     setModalInsertar(true);
   }
-  const insertar =() =>{
+  const insertar = () => {
     var valorInsertar = elementoSeleccionado;
     var dataNueva = data;
     dataNueva.push(valorInsertar);
@@ -48,7 +48,7 @@ function App() {
   return (
     <div className="App-header">
       <h1>Guarda tus Apuntes!</h1>
-      <button className = "btn btn-success" onClick={()=>abrirModalInsertar()}>Insertar
+      <button className="btn btn-success" onClick={() => abrirModalInsertar()}>Insertar
       </button>
       <Table className="table table-bordered">
         <thead>
@@ -67,60 +67,73 @@ function App() {
               <td>{elemento.Apunte}</td>
               <td>{elemento.Fecha}</td>
               <td>{elemento.Fecha}</td>
-              <td><button className="btn btn-danger" onClick = {() => seleccionarElemento(elemento, 'Eliminar')}>Eliminar</button></td>
+              <td><button className="btn btn-danger" onClick={() => seleccionarElemento(elemento, 'Eliminar')}>Eliminar</button></td>
             </tr>
           ))
           }
         </tbody>
       </Table>
-      <Modal isOpen = {modalEliminar}>
+      <Modal isOpen={modalEliminar}>
         <ModalBody>
           ¿Está seguro que desea eliminar el apunte {elementoSeleccionado && elementoSeleccionado.Apunte}?
         </ModalBody>
         <ModalFooter>
-          <button className = "btn btn-danger" onClick={()=>eliminar()}>
+          <button className="btn btn-danger" onClick={() => eliminar()}>
             Aceptar
           </button>
           <button
-            className = "btn btn-secondary"
-            onClick = {() => setModalEliminar(false)}
+            className="btn btn-secondary"
+            onClick={() => setModalEliminar(false)}
           >
             Rechazar
           </button>
         </ModalFooter>
       </Modal>
-      <Modal isOpen = {modalInsertar}>
+      <Modal isOpen={modalInsertar}>
         <ModalHeader>
+          <div>
+            <h3>Agregar Apuntes</h3>
+          </div>
+        </ModalHeader>
           <ModalBody>
-            <div className = "form-group">
+            <div className="form-group">
               <label>Apunte</label>
               <input
-                className = "from-control"
-                type = "text"
-                name = "Apunte"
-                value = {elementoSeleccionado ? elementoSeleccionado.Apunte: ''}
+                className="from-control"
+                type="text"
+                name="Apunte"
+                value={elementoSeleccionado ? elementoSeleccionado.Apunte : ''}
               />
               <br />
               <label>Fecha</label>
               <input
-                className = "form-control"
-                type = "text"
-                name = "Fecha"
-                value = {elementoSeleccionado ? elementoSeleccionado.Fecha: ''}
+                className="form-control"
+                type="text"
+                name="Fecha"
+                value={elementoSeleccionado ? elementoSeleccionado.Fecha : ''}
               />
               <br />
               <label>Etiqueta</label>
               <input
-                className = "form-control"
-                type = "text"
-                name = "Etiqueta"
-                value = {elementoSeleccionado ? elementoSeleccionado.Etiqueta: ''}
+                className="form-control"
+                type="text"
+                name="Etiqueta"
+                value={elementoSeleccionado ? elementoSeleccionado.Etiqueta : ''}
               />
               <br />
             </div>
           </ModalBody>
-        </ModalHeader>
-        
+          <ModalFooter>
+            <button className="btn btn-primary">
+              Agregar Apunte
+          </button>
+            <button
+              className="btn btn-danger"
+              onClick={modalInsertar(false)}
+            >
+              Cancelar
+          </button>
+          </ModalFooter>
       </Modal>
     </div >
   );
